2b7639f9410293553818c27e49e6653e218300ce
Tansong <tansongsong@gmail.com>
2017-04-21 14:02:31 +0800

    使用线程池

--- download.py
+++ download.py
@@ -1,10 +1,12 @@
-import json, os
+import json, os, sys
 import pickle
 from pprint import pprint
+import threading
 from multiprocessing.dummy import Pool as ThreadPool
 from pytube import YouTube
 
 done_video = set()
+NUM_THREAD = 4
 
 def get_video():
     global done_video
@@ -33,22 +35,45 @@ def get_video():
             num_done += 1 
         else:
             tasks.append(x)
-            
-    for x in tasks:
+    print(str(num_done) + '/' + str(num_tasks))
+
+    # multithreading
+
+    lock = threading.Lock()
+    def worker(i):
+        x = tasks.pop(0)
         if x[0] in done_video:
             print(x[0] + ' has been download')
-            num_done += 1
-            continue
+            return
         if os.path.exists('./dataset/' + x[0] + '.mp4'):
             os.remove('./dataset/' + x[0] + '.mp4')
         try:
             download_video(x[0], x[1])
-            num_done += 1
-            print(str(num_done) + '/' + str(num_tasks))
-            with open('done_video.txt', 'a') as done_file:
-                done_file.write(x[0] + '\n')
-        except:
-            pass
+            lock.acquire()
+            try:
+                with open('done_video.txt', 'a') as done_file:
+                    done_file.write(x[0] + '\n')
+            finally:
+                lock.release()
+        except KeyboardInterrupt:
+            print('Keyboard Interrupt')
+            sys.exit(0)
+        finally:
+            return
+            
+    try:
+        from multiprocessing.pool import ThreadPool
+        multi_available = True
+    except ImportError:
+        print('multiprocessing not available, fall back to single threaded encoding')
+        multi_available = False
+
+    if multi_available and NUM_THREAD > 0:
+        p = ThreadPool(NUM_THREAD)
+        p.map(worker, [i for i in range(len(tasks))])
+    else:
+        for _ in range(len(tasks)):
+            worker(_)
 
 def download_video(name, url):
     print(name, url)
